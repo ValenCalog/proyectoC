c21d77a557f3a712cb3272554a360ff707e03c6a
ValenCalog <127156207+ValenCalog@users.noreply.github.com>
2023-11-13 20:10:06 -0300

    commit

--- mainTPI.c
+++ mainTPI.c
@@ -99,6 +99,9 @@ void showaccountcredit();
 void buscarMovimientosUsuario();
 int nroDeTelefonoEsCorrecto(long nroDeTelefono);
 void ListarChoferes();
+void unistlist();
+int existeNroDeUnidad(int nroUnidad);
+
 
 int main() {
 	int opc,confirmar;
@@ -230,7 +233,8 @@ void menuConsultas(){
 		printf("\n7.Buscar chofer o choferes con mas pasajeros en un mes en especifico");
 		printf("\n8.Ver porcentaje de pasajeros que viajan en el primer turno del año");
 		printf("\n9.Listar Choferes");
-		printf("\n10.Volver atras");
+		printf("\n10.Listar unidades");
+		printf("\n11.Volver atras");
 				printf("\nIngrese su opcion:");
 				scanf("%d", &opc);
 				system("cls");
@@ -262,6 +266,9 @@ void menuConsultas(){
 					break;
 				case 9:
 					ListarChoferes();
+					break;
+				case 10:
+					unistlist();
 					break;
 				default:
 					printf("No se ingreso una opcion valida");
