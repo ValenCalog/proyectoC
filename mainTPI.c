469172348d3b82c832a3a6790d6d34c1ca4d52a7
Fabricio Quintana <miguelalfredoq@gmail.com>
2023-11-14 19:43:53 -0300

    .

--- mainTPI.c
+++ mainTPI.c
@@ -581,7 +581,7 @@ int seEncuentraDniChofer(long dni){
 }
 
 void agregarChofer(){
-	int ultimoId, band, existe, aux;
+	int ultimoId, band, existe, aux, c;
 	ultimoId = generarIdChofer();
 	long int dniAux;
 	if(ultimoId==(-1)){
@@ -602,23 +602,12 @@ void agregarChofer(){
 						if((CHOFERES = fopen("choferes.dat", "a+b")) != NULL){
 							while ((c = getchar()) != '\n' && c != EOF);
 							printf("\nIngrese nombre del chofer: ");
-<<<<<<< HEAD
-							fflush(stdin);
-=======
->>>>>>> 409e2e4dbfa34dcf2477a96b6ef94ccb2ebe4910
 							fgets(chofer.NomApe, sizeof(chofer.NomApe), stdin);
 							while ((c = getchar()) != '\n' && c != EOF);
 							printf("\nIngrese direccion: ");
-<<<<<<< HEAD
-							fflush(stdin);
-							fgets(chofer.direccion, sizeof(chofer.direccion), stdin);
-							printf("\nIngrese email: ");
-							fflush(stdin);
-=======
 							fgets(chofer.direccion, sizeof(chofer.direccion), stdin);
 							while ((c = getchar()) != '\n' && c != EOF);
 							printf("\nIngrese email: ");
->>>>>>> 409e2e4dbfa34dcf2477a96b6ef94ccb2ebe4910
 							fgets(chofer.email, sizeof(chofer.email), stdin);
 							printf("\nIngrese fecha de nacimiento ");
 							printf("\nDia: ");
